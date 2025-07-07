5885981fb21766a493b14ec6ebfa2581ec9a76db
Sabbir-Hossain-00 <ms7398037@gmail.com>
2025-07-08 00:20:36 +0600

    create jwt token and set it to cookie

--- index.js
+++ index.js
@@ -3,12 +3,33 @@ const express = require('express')
 const app = express()
 const cors = require("cors")
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const port = 3000
 
 // midleware 
-app.use(cors())
+app.use(cors({
+    origin:["http://localhost:5173"],
+    credentials:true ,
+}))
 app.use(express.json())
+app.use(cookieParser());
 
+
+// verify token 
+const verifyToken = (req , res , next)=>{
+    const token = req.cookies ;
+    if(!token){
+        return res.status(401).send({message : "Unauthorized"})
+    }
+    jwt.verify(token , process.env.JWT_SECRET , (err , decoded)=>{
+        if(err){
+            return res.status(401).send({message : "Unauthorized"})
+        }
+        req.decoded = decoded ;
+        next()
+    })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vlrcl7k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -27,8 +48,27 @@ async function run() {
  
     await client.connect();
 
+ 
+    // jwt token create and set to cookie
+    app.post("/jwt", async(req , res)=>{
+        const {email} = req.body ;
+        const token = jwt.sign({ email}, process.env.JWT_SECRET , {expiresIn : "365d"})
+       
+        res.cookie('token', token ,{
+            httpOnly:true ,
+            secure:false
+        }).send({success : true})
+        
+    })
 
-    
+    // jwt token clear from cookie 
+    app.post("/logout", async(req , res)=>{
+        res.clearCookie('token',{
+            httpOnly: true ,
+            secure:false
+        })
+        res.send({message : "logged out successfully"})
+    })
 
 
 
