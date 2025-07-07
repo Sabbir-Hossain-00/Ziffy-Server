9f9aa608a688b63710fa2acd1d1c1342a9db9276
Sabbir-Hossain-00 <ms7398037@gmail.com>
2025-07-07 20:24:19 +0600

    initail setup done

---
+++ index.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+const express = require('express')
+const app = express()
+const cors = require("cors")
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = 3000
+
+// midleware 
+app.use(cors())
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vlrcl7k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+ 
+    await client.connect();
+
+
+    
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('This is Ziffy')
+})
+
+app.listen(port, () => {
+  console.log(`Ziffy is running on port ${port}`)
+})
